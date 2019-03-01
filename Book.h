67fb3e1447e11321809db221b09725480dd2ceff
Nabeel <nab2320@gmail.com>
2019-02-28 17:20:55 -0800

    edited Book header file

--- Book.h
+++ Book.h
@@ -7,5 +7,57 @@
 #define Book_h
 
 
+class Book
+{
+private:
+    std::string ISBN;
+    std::string Title;
+    std::string Author;
+    std::string Publisher;
+    int quantity;
+    double wholeSaleCost;
+    double RetailPrice;
+
+    
+public:
+    //Constructor
+    Book(){
+        
+    };
+    Book(std::string ISBN, std::string Title, std::string Author, std::string Publisher, int quantity, double wholeSaleCost,  double RetailPrice );
+    Book(const Book &orig);
+    
+    // Destructor
+    ~Book();
+    
+    // Accessor or getters
+    std::string getISBN() const;
+    std::string getTitle() const { return Title; }
+    std::string getAuthor() const { return Author; }
+    int getwholeSaleCost() const { return wholeSaleCost; }
+    double getRetailPrice() const { return RetailPrice() const { return price; }
+        ; }
+    
+    //The accessor for the interator
+    static int getBookCount() { return quantity; }
+    
+    //mutators or setters
+    void setISBN(std::string m) {model = m; }
+    void setTitle(std::string);
+    void setRetailPrice(double);
+
+    
+    //operating overloading
+//    example
+//    friend book operator+(car &left, car&right);
+
+    
+};
+
+
+
+
+
+
 
 #endif /* Book_h */
